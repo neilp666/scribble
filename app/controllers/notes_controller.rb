da91fc9e9705aa6a1072fc38211cdddd69df2b9f
neil patel <neil@myindianweddingapp.com>
2015-01-20 19:53:50 +0000

    Added Header

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,11 +2,10 @@ class NotesController < ApplicationController
 	before_action :find_note, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@notes = Note.all.order("created_at DESC")
+		@notes = Note.where(user_id: current_user)
 	end
 
 	def show
-
 	end
 
 	def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
-
   resources :notes 
 
+  authenticated :user do
+  	root 'notes#index', as: "authenticated_root"
+  end
+
+  root "welcome#index"
 end
