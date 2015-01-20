9e6ff8bc18e24fc2c446dc4d014666d079700f2a
neil patel <neil@myindianweddingapp.com>
2015-01-20 11:17:30 +0000

    Added note views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,33 +1,50 @@
 class NotesController < ApplicationController
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
 
-def index
-new
+	def index
+		@notes = Note.all.order("created_at DESC")
+	end
 
-def show
+	def show
+
+	end
+
+	def new
+		@note = Note.new
+	end
+
+	def create
+		@note = Note.new(note_params)
+
+		if @note.save
+			redirect_to @note
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end 
+
+	def destroy
+	end
+
+	private
+
+
+	def find_note
+		@note = Note.find(params[:id])
+	end
+
+	def note_params
+		params.require(:note).permit(:title, :content)
+	end
+
 end
 
-def new
-end
-
-def create
-end
-
-def destroy
-end
-
-def edit
-end
-
-
-private
-
-def find_note
-end
-
-def note_params
-end
-
-end
 
 
 
